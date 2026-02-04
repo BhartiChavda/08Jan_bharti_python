cd8eb1bb3c0b05247086f0b1575746922c77a317
BhartiChavda <bhartichavda2554@gmail.com>
2026-02-04 18:35:11 +0530

    module 2

--- Assignments/module 2/list.py
+++ Assignments/module 2/list.py
@@ -1,16 +1,25 @@
+#list
 """data=[]
 n =int(input("enter number of subject: "))
 for i in range(n):
     subject=input("enter subject name: ")
     data.append(subject)
 print(data)"""
-    
-data=()
-n =int(input("enter number of city: "))
-for i in range(n):
-    n=input("enter city name: ")
-    data= data+(n,)
 
-print(data)
+
+
+#list example
+student=[]
+studno=int(input("how many student to add: "))
+for i in range(studno):
+    studnm=input("enetr student name: ")
+    student.append(studnm)
+print(student)
+
+search_stud=input("search student name: ")
+if search_stud==studnm:
+    print(search_stud,"student found")
+else:
+    print(search_stud,"student not found")
 
 
